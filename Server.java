14f662ad66bbc7b802c94ee6beaee86e652687ba
Cody Walker <themightycoder@gmail.com>
2019-07-24 00:33:15 -0700

    BUGFIX: removed extra newline between crew members when homies ask where their crew at

--- Server.java
+++ Server.java
@@ -187,7 +187,7 @@ public class Server {
                                 ClientHandler clientHandler =
                                     clients.get(i);
                                 writeMessage(i + ".) " + clientHandler.username
-                                        + " SINCE " + clientHandler.date + "\n");
+                                        + " SINCE " + clientHandler.date);
                             }
                             break;
                     } /* END switch() */
