70ecf1aa26bd1e989bfa00a7c8b3c96ade5a2ea4
Cody Walker <themightycoder@gmail.com>
2019-07-28 23:45:05 -0700

    Start of implementation of chatrooms for server.

---
+++ ChatRoom.java
@@ -0,0 +1,34 @@
+/*
+ * File:        ChatRoom.java
+ * Purpose:     Represents a chat room as a part of the Server.  That way,
+ *              Clients can choose separate chat rooms to communicate
+ *              with only those in that chatroom, rather than everybody
+ *              being forced to share the same space for different
+ *              conversations.
+ */
+
+
+public class ChatRoom {
+
+
+    private String name;
+    private ArrayList<ClientHandler> chatRoomClients;
+
+
+    public ChatRoom(String name) {
+        this.name = name;
+        thiss.chatRoomClients = new ArrayList<ClientHandler>();
+    }
+
+    
+    private void addChatRoomClient(ClientHandler clientHandler) {
+        this.chatRoomClients.add(clientHandler);
+    }
+
+
+    private ArrayList<ClientHandler> getChatRoomClients() {
+        return this.chatRoomClients;
+    }
+
+
+} /* END ChatRoom.java */
